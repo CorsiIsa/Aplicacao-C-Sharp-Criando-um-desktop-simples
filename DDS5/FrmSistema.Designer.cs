ee35a91d0a7b39859a9bb0ca4d33c4a7415dc169
CorsiIsa <icorsi15@gmail.com>
2023-06-26 17:44:47 -0300

    Desktop simples

---
+++ DDS5/FrmSistema.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace DDS5
+{
+    partial class FrmSistema
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSistema));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aplicativosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.microsoftWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.microsoftExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.microsoftPowerPointToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.calculadoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stpData = new System.Windows.Forms.ToolStripStatusLabel();
+            this.stpHora = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.process1 = new System.Diagnostics.Process();
+            this.verificarNúmerosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateTimerPickerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aplicativosToolStripMenuItem,
+            this.programasToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aplicativosToolStripMenuItem
+            // 
+            this.aplicativosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.microsoftWordToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.microsoftExcelToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.microsoftPowerPointToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.calculadoraToolStripMenuItem});
+            this.aplicativosToolStripMenuItem.Name = "aplicativosToolStripMenuItem";
+            this.aplicativosToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.aplicativosToolStripMenuItem.Text = "&Aplicativos";
+            // 
+            // programasToolStripMenuItem
+            // 
+            this.programasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verificarNúmerosToolStripMenuItem,
+            this.pictureBoxToolStripMenuItem,
+            this.comboBoxToolStripMenuItem,
+            this.listBoxToolStripMenuItem,
+            this.timerToolStripMenuItem,
+            this.dateTimerPickerToolStripMenuItem});
+            this.programasToolStripMenuItem.Name = "programasToolStripMenuItem";
+            this.programasToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.programasToolStripMenuItem.Text = "&Programas";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.sairToolStripMenuItem.Text = "&Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // microsoftWordToolStripMenuItem
+            // 
+            this.microsoftWordToolStripMenuItem.Image = global::DDS5.Properties.Resources.word;
+            this.microsoftWordToolStripMenuItem.Name = "microsoftWordToolStripMenuItem";
+            this.microsoftWordToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.microsoftWordToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.microsoftWordToolStripMenuItem.Text = "Microsoft Word";
+            this.microsoftWordToolStripMenuItem.Click += new System.EventHandler(this.microsoftWordToolStripMenuItem_Click);
+            // 
+            // microsoftExcelToolStripMenuItem
+            // 
+            this.microsoftExcelToolStripMenuItem.Image = global::DDS5.Properties.Resources.excel;
+            this.microsoftExcelToolStripMenuItem.Name = "microsoftExcelToolStripMenuItem";
+            this.microsoftExcelToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.microsoftExcelToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.microsoftExcelToolStripMenuItem.Text = "Microsoft Excel";
+            this.microsoftExcelToolStripMenuItem.Click += new System.EventHandler(this.microsoftExcelToolStripMenuItem_Click);
+            // 
+            // microsoftPowerPointToolStripMenuItem
+            // 
+            this.microsoftPowerPointToolStripMenuItem.Image = global::DDS5.Properties.Resources.fpowerpoint;
+            this.microsoftPowerPointToolStripMenuItem.Name = "microsoftPowerPointToolStripMenuItem";
+            this.microsoftPowerPointToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.microsoftPowerPointToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.microsoftPowerPointToolStripMenuItem.Text = "Microsoft PowerPoint";
+            this.microsoftPowerPointToolStripMenuItem.Click += new System.EventHandler(this.microsoftPowerPointToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(235, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(235, 6);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(235, 6);
+            // 
+            // calculadoraToolStripMenuItem
+            // 
+            this.calculadoraToolStripMenuItem.Image = global::DDS5.Properties.Resources.calc;
+            this.calculadoraToolStripMenuItem.Name = "calculadoraToolStripMenuItem";
+            this.calculadoraToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.calculadoraToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.calculadoraToolStripMenuItem.Text = "Calculadora";
+            this.calculadoraToolStripMenuItem.Click += new System.EventHandler(this.calculadoraToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stpData,
+            this.stpHora});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stpData
+            // 
+            this.stpData.Name = "stpData";
+            this.stpData.Size = new System.Drawing.Size(33, 17);
+            this.stpData.Text = "Data";
+            // 
+            // stpHora
+            // 
+            this.stpHora.Name = "stpHora";
+            this.stpHora.Size = new System.Drawing.Size(34, 17);
+            this.stpHora.Text = "Hora";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // verificarNúmerosToolStripMenuItem
+            // 
+            this.verificarNúmerosToolStripMenuItem.Name = "verificarNúmerosToolStripMenuItem";
+            this.verificarNúmerosToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.V)));
+            this.verificarNúmerosToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.verificarNúmerosToolStripMenuItem.Text = "Verificar Números";
+            this.verificarNúmerosToolStripMenuItem.Click += new System.EventHandler(this.verificarNúmerosToolStripMenuItem_Click);
+            // 
+            // pictureBoxToolStripMenuItem
+            // 
+            this.pictureBoxToolStripMenuItem.Name = "pictureBoxToolStripMenuItem";
+            this.pictureBoxToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.P)));
+            this.pictureBoxToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.pictureBoxToolStripMenuItem.Text = "Picture Box";
+            this.pictureBoxToolStripMenuItem.Click += new System.EventHandler(this.pictureBoxToolStripMenuItem_Click);
+            // 
+            // comboBoxToolStripMenuItem
+            // 
+            this.comboBoxToolStripMenuItem.Name = "comboBoxToolStripMenuItem";
+            this.comboBoxToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.C)));
+            this.comboBoxToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.comboBoxToolStripMenuItem.Text = "Combo Box";
+            this.comboBoxToolStripMenuItem.Click += new System.EventHandler(this.comboBoxToolStripMenuItem_Click);
+            // 
+            // listBoxToolStripMenuItem
+            // 
+            this.listBoxToolStripMenuItem.Name = "listBoxToolStripMenuItem";
+            this.listBoxToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.L)));
+            this.listBoxToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.listBoxToolStripMenuItem.Text = "List Box";
+            this.listBoxToolStripMenuItem.Click += new System.EventHandler(this.listBoxToolStripMenuItem_Click);
+            // 
+            // timerToolStripMenuItem
+            // 
+            this.timerToolStripMenuItem.Name = "timerToolStripMenuItem";
+            this.timerToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.T)));
+            this.timerToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.timerToolStripMenuItem.Text = "Timer";
+            this.timerToolStripMenuItem.Click += new System.EventHandler(this.timerToolStripMenuItem_Click);
+            // 
+            // dateTimerPickerToolStripMenuItem
+            // 
+            this.dateTimerPickerToolStripMenuItem.Name = "dateTimerPickerToolStripMenuItem";
+            this.dateTimerPickerToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.D)));
+            this.dateTimerPickerToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.dateTimerPickerToolStripMenuItem.Text = "DateTimerPicker";
+            this.dateTimerPickerToolStripMenuItem.Click += new System.EventHandler(this.dateTimerPickerToolStripMenuItem_Click);
+            // 
+            // FrmSistema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmSistema";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Aplicativos";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aplicativosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem microsoftWordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem microsoftExcelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem microsoftPowerPointToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem calculadoraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stpData;
+        private System.Windows.Forms.ToolStripStatusLabel stpHora;
+        private System.Windows.Forms.Timer timer1;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.ToolStripMenuItem verificarNúmerosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pictureBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comboBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dateTimerPickerToolStripMenuItem;
+    }
+}
+
---
+++ DDS5/FrmSistema.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDS5
+{
+    public partial class FrmSistema : Form
+    {
+        #region Declaração de Variaveis
+        string texto = "Deseja sair do sistema?";
+        string Titulo = "<<<<FINALIZANDO>>>>";
+        MessageBoxButtons botao = MessageBoxButtons.YesNo;
+        MessageBoxIcon icone = MessageBoxIcon.Question;
+        object resposta;
+        #endregion
+
+        public FrmSistema()
+        {
+            InitializeComponent();
+        }
+
+        public void Sair()
+        {
+            this.resposta = MessageBox.Show(this.texto,
+                                            this.Titulo,
+                                            this.botao,
+                                            this.icone);
+            if (resposta.Equals(DialogResult.Yes))
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stpData.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            stpHora.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void microsoftWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process1.StartInfo.FileName = @"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE";
+            process1.Start();
+        }
+
+        private void microsoftExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process1.StartInfo.FileName = @"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE";
+            process1.Start();
+        }
+
+        private void microsoftPowerPointToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process1.StartInfo.FileName = @"C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE";
+            process1.Start();
+        }
+
+        private void calculadoraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process1.StartInfo.FileName = @"C:\Windows\System32\calc.exe";
+            process1.Start();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Sair();
+        }
+
+        private void verificarNúmerosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Win_Exemplo0122.FrmExemplo01 vn = new Win_Exemplo0122.FrmExemplo01();
+            vn.ShowDialog();
+        }
+
+        private void pictureBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Win_EX290822.Form1 PB = new Win_EX290822.Form1();
+            PB.ShowDialog();
+        }
+
+        private void comboBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ATV2_0509.Form1 LB = new ATV2_0509.Form1();
+            LB.ShowDialog();
+        }
+
+        private void timerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ATV3_0509.Form1 Tm = new ATV3_0509.Form1();
+            Tm.ShowDialog();
+        }
+
+        private void dateTimerPickerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ATV4_0509.Form1 Dt = new ATV4_0509.Form1();
+            Dt.ShowDialog();
+        }
+    }
+}
